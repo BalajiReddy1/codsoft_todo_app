ff03291e1a0f832c01ca89501085c3b2e49ade46
Balaji Thukuntala <balajireddy567567.br@gmail.com>
2023-12-06 14:41:07 +0530

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/providers/drawer_index_provider.dart';
+import 'package:todo_app/screens/collections/collections_screen.dart';
+import 'package:todo_app/screens/notes/notes_screen.dart';
+import 'package:todo_app/screens/tasks/tasks_screen.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final index = ref.watch(drawerIndexProvider);
+    return Scaffold(
+      body: [
+        const TasksScreen(),
+        const CollectionsScreen(),
+        const NotesScreen(),
+      ][index],
+    );
+  }
+}
